4968961d3c7fb2aa002e619f2b79c0d6eda58d31
pwirth <wirth@hallowelt.biz>
2021-01-18 13:35:45 +0100

    Fix missing auto read confiration for page creator/editor
    ERM:20110
    [3.1.x]
    
    Change-Id: Ic8c2a717bed22912558bf79fcfbe74cb56c34112

--- includes/ReadConfirmationHooks.php
+++ includes/ReadConfirmationHooks.php
@@ -44,7 +44,7 @@ class ReadConfirmationHooks {
 		if ( !$target ) {
 			return true;
 		}
-		if ( $target->isUserAssigned( $user ) ) {
+		if ( !$target->isUserAssigned( $user ) ) {
 			return true;
 		}
 
