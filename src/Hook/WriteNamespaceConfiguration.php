759a990c4c76fb1f23b7cb79c0bece30d6dfde18
d.savuljesku <d.savuljesku@gmail.com>
2023-05-23 16:14:48 +0200

    Adapt to changes in NamespaceManager DynamicConfig
    Depends-On: I53d16ccc5ca447635d39a143edf3bcbe2f94da5c
    
    [4.x]
    
    ERM31211
    
    Change-Id: I7b7c68d44ca6238c3a3c9ad6c29c8442db7a3f4e
    (cherry picked from commit f5ee7120a1d3dd40fd2a471a9642117ab08436a1)
    (cherry picked from commit 4dc4c33a4fae8748c3bec572004d9f3d75b80dcf)

---
+++ src/Hook/WriteNamespaceConfiguration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\ReadConfirmation\Hook;
+
+use BlueSpice\NamespaceManager\Hook\NamespaceManagerBeforePersistSettingsHook;
+
+class WriteNamespaceConfiguration implements NamespaceManagerBeforePersistSettingsHook {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onNamespaceManagerBeforePersistSettings(
+		array &$configuration, int $id, array $definition, array $mwGlobals
+	): void {
+		$globalValue = $mwGlobals['wgNamespacesWithEnabledReadConfirmation'];
+		if ( isset( $definition['read_confirmation'] ) && $definition['read_confirmation'] === true ) {
+			$configuration['wgNamespacesWithEnabledReadConfirmation'][$id] = true;
+		} elseif ( isset( $definition[ 'read_confirmation' ] ) && $definition['read_confirmation'] === false ) {
+			return;
+		}
+		if ( isset( $globalValue[$id] ) && $globalValue[$id] === true ) {
+			$configuration['wgNamespacesWithEnabledReadConfirmation'][$id] = true;
+		}
+	}
+}
