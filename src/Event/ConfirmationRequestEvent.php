020d830ba0c0979e7700b1231eed51aab79d9fe1
d.savuljesku <d.savuljesku@gmail.com>
2024-03-22 15:47:33 +0100

    Switch to new notifications
    [4.x]
    
    ERM30742
    
    Change-Id: I9f3d8a1a8518f466e0cc413c015b972445f1d719
    (cherry picked from commit 89f05bb779eb3e502e554b3af73c7998f73144ea)

---
+++ src/Event/ConfirmationRequestEvent.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace BlueSpice\ReadConfirmation\Event;
+
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserIdentity;
+use Message;
+use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
+use MWStake\MediaWiki\Component\Events\TitleEvent;
+use Title;
+
+class ConfirmationRequestEvent extends TitleEvent {
+
+	/**
+	 * @var array
+	 */
+	protected $targetUsers;
+
+	/**
+	 * @param UserIdentity $actor
+	 * @param Title $title
+	 * @param array $targetUsers
+	 */
+	public function __construct( UserIdentity $actor, Title $title, array $targetUsers ) {
+		parent::__construct( $actor, $title );
+		$this->targetUsers = $targetUsers;
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getKeyMessage(): Message {
+		return Message::newFromKey( 'readconfirmation-event-confirmation-request-key-desc' );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getMessageKey(): string {
+		return 'readconfirmation-event-confirmation-request';
+	}
+
+	/**
+	 * @return array|null
+	 */
+	public function getPresetSubscribers(): ?array {
+		return $this->targetUsers;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return 'bs-rc-request';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getLinksIntroMessage( IChannel $forChannel ): ?Message {
+		return Message::newFromKey( 'readconfirmation-event-confirmation-links-intro' );
+	}
+
+	/**
+	 * @param UserIdentity $agent
+	 * @param MediaWikiServices $services
+	 * @param array $extra
+	 * @return array
+	 */
+	public static function getArgsForTesting(
+		UserIdentity $agent, MediaWikiServices $services, array $extra = []
+	): array {
+		return array_merge( parent::getArgsForTesting( $agent, $services, $extra ), [
+			[ $extra['targetUser'] ?? $services->getUserFactory()->newFromName( 'WikiSysop' ) ]
+		] );
+	}
+}
