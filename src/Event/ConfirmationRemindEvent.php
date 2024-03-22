89f05bb779eb3e502e554b3af73c7998f73144ea
d.savuljesku <d.savuljesku@gmail.com>
2024-03-22 15:47:33 +0100

    Switch to new notifications
    [4.x]
    
    ERM30742
    
    Change-Id: I9f3d8a1a8518f466e0cc413c015b972445f1d719

---
+++ src/Event/ConfirmationRemindEvent.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BlueSpice\ReadConfirmation\Event;
+
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserIdentity;
+use Message;
+use MWStake\MediaWiki\Component\Events\BotAgent;
+use Title;
+
+class ConfirmationRemindEvent extends ConfirmationRequestEvent {
+
+	/**
+	 * @param Title $title
+	 * @param array $targetUsers
+	 */
+	public function __construct( Title $title, array $targetUsers ) {
+		parent::__construct( new BotAgent(), $title, $targetUsers );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getKeyMessage(): Message {
+		return Message::newFromKey( 'readconfirmation-event-confirmation-remind-key-desc' );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getMessageKey(): string {
+		return 'readconfirmation-event-confirmation-remind';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return 'bs-rc-remind';
+	}
+
+	/**
+	 * @param UserIdentity $agent
+	 * @param MediaWikiServices $services
+	 * @param array $extra
+	 * @return array
+	 */
+	public static function getArgsForTesting(
+		UserIdentity $agent, MediaWikiServices $services, array $extra = []
+	): array {
+		return [
+			$extra['title'] ?? $services->getTitleFactory()->newMainPage(),
+			[ $extra['targetUser'] ?? $services->getUserFactory()->newFromName( 'WikiSysop' ) ]
+		];
+	}
+}
