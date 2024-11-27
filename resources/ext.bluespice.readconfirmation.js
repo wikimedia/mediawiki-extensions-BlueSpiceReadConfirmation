5fd2d4408a48676dc5ac9bf7f30595fddf5e0616
hslater <hamish.slater@hallowelt.com>
2024-11-27 14:25:30 +0100

    Migrate from ExtJS to OOJS
    ERM39447
    
    [REL1_43, master]
    
    Change-Id: If49d631544f806cd46fff51a730ccefc590ecc7f

--- resources/ext.bluespice.readconfirmation.js
+++ resources/ext.bluespice.readconfirmation.js
@@ -1,9 +1,9 @@
-(function( mw, $, bs, d, undefined ){
+( function ( mw, $, bs ) {
 
-	function _buildMessage( curPageId ) {
+	function buildMessage( curPageId ) {
 
-		//TODO: Use Mustache template
-		var html =
+		// TODO: Use Mustache template
+		const html =
 			'<div id="bs-readconfirmation-container">' +
 				'<h3>{0}</h3>' +
 				'<table><tr>' +
@@ -18,45 +18,41 @@
 				'</tr><table>' +
 			'</div>';
 
-
-		var $message = $( html.format(
+		const $message = $( html.format(
 			mw.message( 'bs-readconfirmation-confirm-read-heading' ).plain(),
 			mw.message( 'bs-readconfirmation-confirm-read-checkbox-label' ).plain(),
 			mw.message( 'bs-readconfirmation-confirm-read-button-label' ).plain(),
 			mw.message( 'bs-readconfirmation-confirm-read-aria-description' ).plain()
-		));
+		) );
 
-		var $button = $message.find( '#bs-rc-btn-ack' );
-		$button.on( 'click', function() {
-			mw.loader.using( 'ext.bluespice.extjs', function() {
-				Ext.Loader.setPath( 'BS.ReadConfirmation', mw.config.get('wgScriptPath') + '/extensions/BlueSpiceReadConfirmation/resources/BS.ReadConfirmation' );
-				Ext.require( 'BS.ReadConfirmation.action.ApiReadTaskConfirm', function() {
-					var action = new BS.ReadConfirmation.action.ApiReadTaskConfirm({
-						pageId: curPageId
-					});
-					action.execute()
-						.fail(function( xhr, data, response ){
-							bs.util.alert( 'bs-rc-confirm', {
-								text: response.message
-							});
-						})
-						.done(function(){
-							bs.alerts.remove( 'bs-readconfirmation-info' );
-						});
+		const $button = $message.find( '#bs-rc-btn-ack' );
+		$button.on( 'click', async () => {
+			await mw.loader.using( 'mediawiki.api' );
+			const api = new mw.Api();
+
+			try {
+				await api.postWithToken( 'csrf', {
+					action: 'bs-readconfirmation-tasks',
+					task: 'confirm',
+					taskData: JSON.stringify( { pageId: curPageId } )
 				} );
-			} );
+				bs.alerts.remove( 'bs-readconfirmation-info' );
+			} catch ( error ) {
+				bs.util.alert( 'bs-rc-confirm', {
+					text: error.message
+				} );
+			}
 		} );
 
-		var $checkbox = $message.find( '#bs-rc-cb-ack' );
-		$checkbox.on( 'change', function() {
-			$button.prop( 'disabled', !$(this).is(':checked') );
-			if( $(this).is(':checked') ) {
-				$button.css( 'visibility', 'visible');
+		const $checkbox = $message.find( '#bs-rc-cb-ack' );
+		$checkbox.on( 'change', function () {
+			$button.prop( 'disabled', !$( this ).is( ':checked' ) );
+			if ( $( this ).is( ':checked' ) ) {
+				$button.css( 'visibility', 'visible' );
+			} else {
+				$button.css( 'visibility', 'hidden' );
 			}
-			else {
-				$button.css( 'visibility', 'hidden');
-			}
-		});
+		} );
 
 		bs.alerts.add(
 			'bs-readconfirmation-info',
@@ -66,34 +62,33 @@
 	}
 
 	bs.util.registerNamespace( 'bs.readconfirmation' );
-	bs.readconfirmation.init = function() {
-		if( mw.config.get( 'wgAction', 'view' ) !== 'view' ) {
+	bs.readconfirmation.init = function () {
+		if ( mw.config.get( 'wgAction', 'view' ) !== 'view' ) {
 			return;
 		}
 
-		var curPageId = mw.config.get( 'wgArticleId' );
-		if( curPageId < 1 ) { //SpecialPages ...
+		const curPageId = mw.config.get( 'wgArticleId' );
+		if ( curPageId < 1 ) { // SpecialPages ...
 			return;
 		}
-		var data = {
+		const data = {
 			pageId: curPageId
 		};
 
-		mw.hook( 'readconfirmation.check.request.before' ).fire(data);
+		mw.hook( 'readconfirmation.check.request.before' ).fire( data );
 
-		//We use a normal BS Tasks Api abstraction here even though there is
-		//a BS.ReadConfirmation.action.ApiReadTaskCheck class.
-		//This is to avoid loading 'ext.bluespice.extjs' if not necessary
 		bs.api.tasks.execSilent( 'readconfirmation', 'check', data )
-			.done(function( response, xhr ){
-				if( response.success && response.payload.userHasConfirmed === false ) {
-					mw.loader.using( 'mediawiki.ui.checkbox' ).done( function() {
-						_buildMessage( curPageId );
-					});
+			.done( function ( response ) {
+				if ( response.success && response.payload.userHasConfirmed === false ) {
+					mw.loader.using( 'mediawiki.ui.checkbox' ).done( function () {
+						buildMessage( curPageId );
+					} );
 				}
-			});
+			} );
 	};
 
-	setTimeout(function(){$(bs.readconfirmation.init)}, 1000);
+	setTimeout( function () {
+		$( bs.readconfirmation.init );
+	}, 1000 );
 
-})( mediaWiki, jQuery, blueSpice, document );
+}( mediaWiki, jQuery, blueSpice ) );
