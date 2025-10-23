1df5d8aa95fe03f977c5165683536f13ca23529e
Hua Jing <hua.jing@hallowelt.com>
2025-10-23 19:49:17 +0200

    Fix page count that mistriggers attention indicator
    only in those namespaces with read confirmation enabled,
    assignments on pages need to be counted for indicator
    
    ERM44746
    
    [5.1.x]
    
    Change-Id: I32a15fac9562bbad09334685d4a690492c24e911

--- src/AttentionIndicator/ReadConfirmation.php
+++ src/AttentionIndicator/ReadConfirmation.php
@@ -10,6 +10,8 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\TitleFactory;
 use MediaWiki\User\User;
 use MediaWiki\User\UserGroupManager;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\IResultWrapper;
 use Wikimedia\Rdbms\LoadBalancer;
 
 class ReadConfirmation extends AttentionIndicator {
@@ -35,6 +37,11 @@ class ReadConfirmation extends AttentionIndicator {
 	protected $titleFactory = null;
 
 	/**
+	 * @var IDatabase
+	 */
+	private $dbr;
+
+	/**
 	 * @param string $key
 	 * @param Config $config
 	 * @param User $user
@@ -50,6 +57,7 @@ class ReadConfirmation extends AttentionIndicator {
 	) {
 		$this->mechanismFactory = $mechanismFactory;
 		$this->lb = $lb;
+		$this->dbr = $lb->getConnection( DB_REPLICA );
 		$this->userGroupManager = $userGroupManager;
 		$this->titleFactory = $titleFactory;
 		parent::__construct( $key, $config, $user );
@@ -95,30 +103,60 @@ class ReadConfirmation extends AttentionIndicator {
 	}
 
 	/**
+	 * @return array
+	 */
+	protected function getEnabledNamespaces(): array {
+		$namespaces = $this->config->get( 'NamespacesWithEnabledReadConfirmation' );
+		return is_array( $namespaces )
+			? array_keys( array_filter(
+				$namespaces,
+				fn ( $enabled, $nsId ) => is_int( $nsId ) && $enabled === true,
+				ARRAY_FILTER_USE_BOTH
+			) ) : [];
+	}
+
+	/**
+	 * @param array $conditions
+	 * @return IResultWrapper
+	 */
+	private function selectPageIds( array $conditions ) {
+		return $this->dbr->newSelectQueryBuilder()
+			->fields( [ 'pa_page_id' ] )
+			->table( 'bs_pageassignments' )
+			->join( 'page', null, 'pa_page_id = page_id' )
+			->where( array_merge( $conditions, [
+				'page_namespace' => $this->getEnabledNamespaces()
+			] ) )
+			->distinct()
+			->caller( __METHOD__ )
+			->fetchResultSet();
+	}
+
+	/**
 	 * @return int
 	 */
 	protected function doIndicationCount(): int {
 		$count = 0;
-		$userGroups = $this->userGroupManager->getUserGroups( $this->user );
-		$ids = [];
-		$res = $this->lb->getConnection( DB_REPLICA )->select(
-			'bs_pageassignments',
-			[ 'pa_page_id' ],
-			[ 'pa_assignee_key' => array_merge( [ $this->user->getName() ], $userGroups ) ],
-			__METHOD__,
-			[ 'DISTINCT' ]
-		);
-		foreach ( $res as $row ) {
-			$ids[] = $row->pa_page_id;
+		if ( $this->getEnabledNamespaces() === [] ) {
+			return $count;
 		}
-		$res = $this->lb->getConnection( DB_REPLICA )->select(
-			'bs_pageassignments',
-			[ 'pa_page_id' ],
-			[ 'pa_assignee_type' => 'everyone' ],
-			__METHOD__
-		);
-		foreach ( $res as $row ) {
-			$ids[] = $row->pa_page_id;
+		$cases = [
+			[
+				'pa_assignee_key' => [ $this->user->getName() ],
+				'pa_assignee_type' => 'user'
+			],
+			[
+				'pa_assignee_key' => $this->userGroupManager->getUserGroups( $this->user ),
+				'pa_assignee_type' => 'group'
+			],
+			[ 'pa_assignee_type' => 'everyone' ]
+		];
+		$ids = [];
+		foreach ( $cases as $conditions ) {
+			$res = $this->selectPageIds( $conditions );
+			foreach ( $res as $row ) {
+				$ids[] = $row->pa_page_id;
+			}
 		}
 		$ids = array_unique( $ids );
 
